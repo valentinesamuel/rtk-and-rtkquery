4c3fd1d657808c8ea6357aae619fba218ed91383
valentinesamuel <valentinesamuel2580@gmail.com>
2023-04-09 00:42:25 +0100

    FEAT: Updating the data and count

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,20 @@ import { useGetPostsQuery } from "./app/features/postSlice";
 import { useAppSelector } from "./utils/state-dispatch";
 
 function App() {
-  const { data, isLoading,isError } = useGetPostsQuery();
-console.log(data);
+  const { data, isLoading, isError } = useGetPostsQuery("getPosts");
+  console.log(data);
 
   const res = useAppSelector((state) => state.postSlice);
   return (
     <div className="App">
+      <h2>{res.count}</h2>
       {JSON.stringify(res)}
       <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      {/* <h2>{data.count}</h2> */}
       {data && JSON.stringify(data)}
       {isLoading && <p>Loading...</p>}
       {isError && <p>Error</p>}
--- src/app/features/postSlice.ts
+++ src/app/features/postSlice.ts
@@ -1,24 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { apiSlice } from "./apiSlice";
 
-const initialState = [
-  {
-    userId: 1,
-    id: 1,
-    title:
-      "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-    body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-  },
-  {
-    userId: 2,
-    id: 2,
-    title: "qui est esse",
-    body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
-  },
-];
+const initialState = {
+  posts: [
+    {
+      userId: 1,
+      id: 1,
+      title:
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+    },
+  ],
+  count: 0,
+};
 
 export const postApiSlice = apiSlice.injectEndpoints({
-
   endpoints: (builder) => ({
     getPosts: builder.query({
       query: () => "/posts",
@@ -33,15 +29,10 @@ export const postSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder.addMatcher(
-      postApiSlice.endpoints.getPosts.matchPending,
-      (state, action) => {
-        state = [];
-      }
-    );
-    builder.addMatcher(
       postApiSlice.endpoints.getPosts.matchFulfilled,
       (state, action) => {
-        state = action.payload;
+        state.posts = action.payload;
+        state.count = action.payload.length;
       }
     );
   },
