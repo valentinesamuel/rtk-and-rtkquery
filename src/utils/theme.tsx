3e9dfefb59712a1893e81ac1765e5c3b8183eab1
valentinesamuel <valentinesamuel2580@gmail.com>
2023-04-11 12:49:03 +0100

    FEAT: Added MUI theme

--- src/utils/theme.tsx
+++ src/utils/theme.tsx
@@ -1,4 +1,26 @@
 import { createTheme } from "@mui/material/styles";
 
 // Define theme settings
+export const lightTheme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#E1E5EE",
+    },
+    secondary: {
+      main: "#2A324B",
+    },
+  },
+});
 
+export const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#2A324B",
+    },
+    secondary: {
+      main: "#E1E5EE",
+    },
+  },
+});
